eb9bce8702a0a8a21fc1a79d873b0f7e4d3553c6
Konstantin Urban <kontenurban@gmail.com>
2019-11-25 21:08:06 +0530

    init

---
+++ src/app/co-work/co-work.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuService } from '../_services/menu.service';
+
+
+@Component({
+  selector: 'app-co-work',
+  templateUrl: './co-work.component.html',
+  styleUrls: ['./co-work.component.scss']
+})
+export class CoWorkComponent implements OnInit {
+
+  constructor(
+    private menu : MenuService
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
