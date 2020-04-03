5db9acfea75851b07346c70c0c9520299a60df38
Konstantin Urban <kontenurban@gmail.com>
2020-04-03 20:27:34 +0200

    contentful init

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuService } from '../_services/menu.service';
-
+import { ContentfulService } from '../_services/contentful.service';
+import { Entry } from 'contentful';
 
 @Component({
   selector: 'app-gallery',
@@ -8,12 +9,17 @@ import { MenuService } from '../_services/menu.service';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
+  exhibits : Entry<any>[] = [];
 
   constructor(
-    public menu : MenuService
+    public menu : MenuService,
+    private contentfulService : ContentfulService
   ) { }
 
   ngOnInit() {
+    this.contentfulService.getExhibits()
+    .then(exhibits => this.exhibits = exhibits)
+    .then(exhibits => console.log('Array of Exhibits:', this.exhibits));
   }
 
 }
