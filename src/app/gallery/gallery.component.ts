e48a134db50d96cba342698f7c362be817187c5d
Konstantin Urban <kontenurban@gmail.com>
2020-04-04 14:23:05 +0200

    swiper added

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { MenuService } from '../_services/menu.service';
 import { ContentfulService } from '../_services/contentful.service';
 import { Entry } from 'contentful';
+declare let Swiper: any;
 
 @Component({
   selector: 'app-gallery',
@@ -9,17 +10,38 @@ import { Entry } from 'contentful';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
-  exhibits : Entry<any>[] = [];
+  exhibits: Entry<any>[] = [];
 
   constructor(
-    public menu : MenuService,
-    private contentfulService : ContentfulService
-  ) { }
+    public menu: MenuService,
+    private contentfulService: ContentfulService
+  ) {
+    new Swiper('.swiper-container', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true
+    });
+  }
 
   ngOnInit() {
     this.contentfulService.getExhibits()
-    .then(exhibits => this.exhibits = exhibits)
-    .then(exhibits => console.log('Array of Exhibits:', this.exhibits));
+      .then(exhibits => this.exhibits = exhibits)
+      .then(exhibits => console.log('Array of Exhibits:', this.exhibits));
   }
 
+  ngAfterViewInit() {
+    setTimeout(function() {
+      new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        nextButton: '.swiper-button-next',
+        direction: 'horizontal',
+        loop: true,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        paginationType: 'fraction',
+        preloadImages: false,
+        lazy: true
+      });
+    }, 3000);
+  }
 }
