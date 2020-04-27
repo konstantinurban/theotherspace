0e1512475cf10c2c4714fdb74fe419ec5788f6e0
Konstantin Urban <kontenurban@gmail.com>
2020-04-27 09:52:38 +0200

    added video timelapse

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { MenuService } from '../_services/menu.service';
 
 @Component({
@@ -7,13 +7,30 @@ import { MenuService } from '../_services/menu.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  isPlaying: boolean = false;
+  buttonShown : boolean = false;
+  @ViewChild("introVideo", { static: false }) introVideo: ElementRef;
 
   constructor(
-    public menu : MenuService
+    public menu: MenuService
   ) {
- }
+  }
 
   ngOnInit() {
   }
 
+  playVideo() {
+    this.buttonShown = false;
+    const video: HTMLVideoElement = this.introVideo.nativeElement;
+    video.play();
+    this.isPlaying = true;
+  }
+
+  videoEnded() {
+    this.isPlaying = false;
+  }
+
+  showPlay() {
+    this.buttonShown = true;
+  }
 }
