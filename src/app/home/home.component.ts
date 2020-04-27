e8d18c3594f11ee02914cf750e99851c91d90810
Konstantin Urban <kontenurban@gmail.com>
2020-04-27 23:29:10 +0200

    vid controls

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { MenuService } from '../_services/menu.service';
+import { IsDesktopService } from '../_services/is-desktop.service';
 
 @Component({
   selector: 'app-home',
@@ -8,20 +9,26 @@ import { MenuService } from '../_services/menu.service';
 })
 export class HomeComponent implements OnInit {
   isPlaying: boolean = false;
-  buttonShown : boolean = false;
+  buttonShown: boolean = false;
+  isDesktop: boolean;
+  isMuted: boolean = true;
   @ViewChild("introVideo", { static: false }) introVideo: ElementRef;
 
   constructor(
-    public menu: MenuService
+    public menu: MenuService,
+    public isDesktopService: IsDesktopService
   ) {
   }
 
   ngOnInit() {
+    this.isDesktop = this.isDesktopService.checkIfDesktop();
+    console.log("init", this.isMuted);
   }
 
   playVideo() {
     this.buttonShown = false;
     const video: HTMLVideoElement = this.introVideo.nativeElement;
+    video.muted = true;
     video.play();
     this.isPlaying = true;
   }
@@ -32,5 +39,22 @@ export class HomeComponent implements OnInit {
 
   showPlay() {
     this.buttonShown = true;
+  }
+
+  endVideo() {
+    const video: HTMLVideoElement = this.introVideo.nativeElement;
+    video.pause();
+    this.isPlaying = false;
+  }
+
+  unmuteVideo() {
+    const video: HTMLVideoElement = this.introVideo.nativeElement;
+    if (this.isMuted) {
+      video.muted = false;
+      this.isMuted = false;
+    } else if (!this.isMuted) {
+      video.muted = true;
+      this.isMuted = true;
+    }
   }
 }
