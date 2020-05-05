e46a4ddff5ee9acc3f84382d98dc3aec8954886d
Konstantin Urban <kontenurban@gmail.com>
2020-05-05 14:52:45 +0200

    Cowork and cafe

--- src/app/cafe/cafe.component.ts
+++ src/app/cafe/cafe.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { MenuService } from '../_services/menu.service';
+declare let Swiper: any;
 
 @Component({
   selector: 'app-cafe',
@@ -9,11 +10,34 @@ import { MenuService } from '../_services/menu.service';
 export class CafeComponent implements OnInit {
 
   constructor(
-    public menu : MenuService
+    public menu: MenuService
   ) {
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    setTimeout(function() {
+      new Swiper('.cafe-swiper-container', {
+        slidesPerView: 1,
+        direction: 'horizontal',
+        loop: true,
+        speed: 600,
+        effect: 'fade',
+        fadeEffect: {
+          crossFade: true
+        },
+        navigation: {
+          nextEl: '.swiper-item-next',
+          prevEl: '.swiper-item-prev'
+        },
+        // pagination: {
+        //   el: '.gallery-swiper-pagination',
+        //   type: 'fraction',
+        // },
+      });
+    }, 3000);
   }
 
 }
